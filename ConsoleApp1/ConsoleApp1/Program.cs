b767569413429f595cf0fb83a7cc5a7724c3480a
Stivens Espinoza Reina <stivens70espinoza@gmail.com>
2022-04-09 08:43:26 -0500

    Aprendiendo C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your name: ");
+            var nombre=Console.ReadLine();
+            Console.WriteLine($"Thanks for register, {nombre}");
+        }
+    }
+}
---
+++ CursoCsharp/CursoCsharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CursoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string nombre = "Stivens";
+            var apellido = "Espinoza";
+
+            Console.WriteLine($"Hello {nombre} {apellido}");
+            Console.WriteLine($"Mi nombre tiene {nombre.Length} letras");
+        }
+    }
+}
